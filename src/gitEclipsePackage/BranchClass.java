7f22e6cff2f34eec1da6cdcf57a7da2742dbf789
admin <admin@DESKTOP-IG7U0S5>
2021-01-05 16:04:24 +0530

    BranchCommitFirst

---
+++ src/gitEclipsePackage/BranchClass.java
@@ -0,0 +1,10 @@
+package gitEclipsePackage;
+
+public class BranchClass {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing Branch as well as import from Git");
+
+	}
+
+}
