8c29238c2c742195c49ac741d7ffbd5275cb831f
admin <admin@DESKTOP-IG7U0S5>
2021-01-05 16:24:46 +0530

    BranchClassCommitSecond

--- src/gitEclipsePackage/BranchClass.java
+++ src/gitEclipsePackage/BranchClass.java
@@ -4,6 +4,8 @@ public class BranchClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Testing Branch as well as import from Git");
+		System.out.println("Added another line");
+
 
 	}
 
